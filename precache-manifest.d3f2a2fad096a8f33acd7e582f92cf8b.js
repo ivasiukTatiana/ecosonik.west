9339b3a9fcce8b18eb6bed112198509a261a921a
Tetiana Mysiuk <tivasyuk@i.ua>
2019-12-04 16:55:58 +0200

    Updates

---
+++ precache-manifest.d3f2a2fad096a8f33acd7e582f92cf8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c558e9acfc2d34b13f899fe49ba65ca1",
+    "url": "/github.io/ecosonik.west/index.html"
+  },
+  {
+    "revision": "780891dea855eb8e4254",
+    "url": "/github.io/ecosonik.west/static/css/main.a39c386b.chunk.css"
+  },
+  {
+    "revision": "1a248d90416c6ab2c00a",
+    "url": "/github.io/ecosonik.west/static/js/2.87d1adf6.chunk.js"
+  },
+  {
+    "revision": "780891dea855eb8e4254",
+    "url": "/github.io/ecosonik.west/static/js/main.75ca95ce.chunk.js"
+  },
+  {
+    "revision": "b64845b8eb683cccf9c7",
+    "url": "/github.io/ecosonik.west/static/js/runtime-main.121d51c7.js"
+  },
+  {
+    "revision": "ec666c80c5deb9d02ca71abd8e2dd23d",
+    "url": "/github.io/ecosonik.west/static/media/fon.ec666c80.jpg"
+  },
+  {
+    "revision": "a928e7c98160734a2a225aeb0b51304a",
+    "url": "/github.io/ecosonik.west/static/media/logo.a928e7c9.jpg"
+  }
+]);
